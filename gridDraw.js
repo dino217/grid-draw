b094eec153b7ed53405a7fe984dc8f0bf79ca32e
Brandon <brob217@gmail.com>
2015-10-11 17:43:38 -0700

    Drawing ability added. Functioning reset button added.

--- gridDraw.js
+++ gridDraw.js
@@ -1,20 +1,39 @@
 var $square = $('<div class="divBlank"></div>');
 
 var setSquareWidth = function(gridNum){
-  return 900 / gridNum;
+  return Math.floor(900 / gridNum);
 }
 
-$("document").ready(function(){
-  var gridCount = 16;
-  var squareWidth = setSquareWidth(gridCount);
-  for (x = 1; x <= gridCount; x++){
-    for (i = 1; i <= gridCount; i++){
-      $("#container").append("<div class ='divBlank'></div>");
-    }
-    $("#container").append("<br>")
-  }
-  $(".divBlank").outerWidth(squareWidth);
-  $(".divBlank").outerHeight(squareWidth);
 
+
+$("document").ready(function(){
+
+  var ready = function(gridCount){
+    $("#container").empty();
+    var squareWidth = setSquareWidth(gridCount);
+    for (x = 1; x <= gridCount; x++){
+      for (i = 1; i <= gridCount; i++){
+        $("#container").append("<div class ='divBlank'></div>");
+      }
+      $("#container").append("<br>")
+    }
+    $(".divBlank").outerWidth(squareWidth);
+    $(".divBlank").outerHeight(squareWidth);
+    $(".divBlank").mouseenter(function(){
+      $(this).css("background-color", "#000");
+    });
+  };
+
+  ready(16);
+
+  $("#resetbutton").click(function(){
+    var count = prompt("Give desired width of drawing board.")
+    if ( isNaN(count) ){
+      alert("Please enter a number.");
+      return false;
+    } else {
+      ready(count);
+    }
+  });
 
 });
